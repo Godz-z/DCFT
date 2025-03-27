e18a8ff7410009cc67ee22dede6e09bc64a54bca
Jia-Chen Zhang <140873323+Godz-z@users.noreply.github.com>
2025-03-27 15:50:39 +0800

    Add files via upload

---
+++ COMMAND/runall.sh
@@ -0,0 +1,9 @@
+bash ./COMMAND/run_debertav3_cola.sh
+bash ./COMMAND/run_debertav3_mnli.sh
+bash ./COMMAND/run_debertav3_mrpc.sh
+bash ./COMMAND/run_debertav3_qnli.sh
+bash ./COMMAND/run_debertav3_qqp.sh
+bash ./COMMAND/run_debertav3_rte.sh
+bash ./COMMAND/run_debertav3_sst2.sh
+bash ./COMMAND/run_debertav3_stsb.sh
+
